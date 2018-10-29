f06893faaba181145da1a24715ed04c60da5a2f4
Ben <bconrad18@gmail.com>
2018-10-28 22:41:00 -0500

    adding details to card view

--- client/src/components/FlightContainer/FlightCard/FlightCard.jsx
+++ client/src/components/FlightContainer/FlightCard/FlightCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, Icon } from 'semantic-ui-react'
+import {Card, Icon} from 'semantic-ui-react';
+import {FaCalendarAlt} from 'react-icons/fa';
 import moment from 'moment';
 
 function formatDate(date) {
@@ -12,23 +13,37 @@ const FlightCard = ({launch}) => {
         <div className={'flight-card-top'}>
           <div>
             <div className={'success-container'}>
-              <span id={'launch-status'}>STATUS: </span>{launch.success ? (<Icon name='checkmark' color="green" style={{fontSize:'16px'}}/>):<Icon name='remove' color="red" style={{fontSize:'18px'}}/>}
+              <span id={'launch-status'}>STATUS: </span>{launch.success ? (
+                <Icon name='checkmark' color="green"
+                      style={{fontSize: '16px'}}/>) : <Icon name='remove'
+                                                            color="red"
+                                                            style={{fontSize: '18px'}}/>}
             </div>
           </div>
 
           <div className={'flight-card-number'}>
             {`Flight # ${launch.flight}`}
           </div>
-          <div className={'flight-card-date'}>
-            {formatDate(launch.date)}
+          <div className={'divider'}/>
+          <div className={'flight-card-name'}>
+            {launch.rocket.rocket_name}
           </div>
           <div className={'flight-card-image-container'}>
             <img src={launch.missionPatch} className={'flight-card-image'}/>
           </div>
         </div>
         <div className={'flight-card-bottom'}>
-          <div className={'flight-card-name'}>
-            {launch.rocket.rocket_name}
+          <div className={'flight-card-date bottom-row'}>
+            <span id={'calender-date'}><FaCalendarAlt
+                className={'descriptor'}/>Date:</span>{formatDate(launch.date)}
+          </div>
+          <div className={'bottom-row'}>
+            <div className={'flight-card-details'}><span
+                className={'descriptor'}>{'Details:'}</span>{launch.details}</div>
+          </div>
+          <div className={'bottom-row'}>
+            <div className={'flight-card-location'}>
+              <span className={'descriptor'}>{'Site:'}</span>{launch.launchSite}</div>
           </div>
         </div>
       </div>
